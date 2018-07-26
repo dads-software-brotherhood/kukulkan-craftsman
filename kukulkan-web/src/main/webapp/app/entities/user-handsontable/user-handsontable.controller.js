27921486c50acbc1c45a29cbdbbae627a031d9fa
Roberto Villarejo Martínez <robertovillarejo@outlook.com>
2018-07-26 12:05:07 -0500

    Directly pass settings object from service to hot instance

--- kukulkan-web/src/main/webapp/app/entities/user-handsontable/user-handsontable.controller.js
+++ kukulkan-web/src/main/webapp/app/entities/user-handsontable/user-handsontable.controller.js
@@ -14,29 +14,8 @@
       var hotInstance = hotRegisterer.getInstance('my-handsontable');
 
       UserHandsontable.getData().then(function (response) {
-        hotInstance.updateSettings({
-          data: response.data.data,
-          columns: response.data.columns,
-          colHeaders: response.data.colHeaders,
-          rowHeaders: true,
-          afterChange: true,
-          dropdownMenu: true,
-          filters: true,
-          height: 440,
-          contextMenu: true,
-          minSpareRows: true,
-          columnSorting: true,
-          colWidths: 140,
-          rowHeights: 25,
-          filters: true,
-          minRows: 20,
-          autoRowSize: true,
-          manualColumnMove: true,
-          manualRowMove: true,
-          fillHandle: {
-            autoInsertRow: false,
-          }
-        });
+        //Overwrite settings object from service
+        hotInstance.updateSettings(response.data);
       });
     });
   }
